8d2eadbc490f6a7bf2a62f7bfbbb2ee0218cca40
Pathak <pratik.pathak@amecfw.com>
2020-09-11 10:55:43 -0400

    Made changes to app

--- application.py
+++ application.py
@@ -4,7 +4,9 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return "Hello World!"
+    return "Hello Pratik!"
+
+
 
 if __name__ == '__main__':
     application.debug = True
